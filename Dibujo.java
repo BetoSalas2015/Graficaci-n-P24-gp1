c58b95d18c310f7c5db97bbf2f236d04415fbc38
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2024-01-30 12:36:39 -0600

    Sesión 7 - 29/01/2024

--- Dibujo.java
+++ Dibujo.java
@@ -1,5 +1,5 @@
 import java.awt.*;
-import java.awt.geom.*;
+ import java.awt.geom.*;
 import java.util.Vector;
 
 public class Dibujo extends Canvas {
@@ -21,8 +21,31 @@ public class Dibujo extends Canvas {
             vectorPuntos = vector;
     }
 
+    private void planoCoordenado(Graphics gc) {
+        Graphics2D g2d = (Graphics2D) gc;
+
+        g2d.setColor( new Color(0x77BF9D));
+        int h = getSize().height / 2;
+        int w = getSize().width / 2;
+
+        gc.translate(w, h);
+
+        g2d.setStroke( new BasicStroke(2.0f));
+        setBackground( new Color( 0x5EFFB3 ));
+
+        for (int i = 0; i > -w; i-= 20) {
+            g2d.draw(new Line2D.Float(i,-h,i,h));
+        }
+        for (int i = 0; i > -h; i-= 20) {
+            g2d.draw(new Line2D.Float(-w, i, w, i));
+        }
+
+
+    }
+
     public void paint(Graphics gc) {
-        int i;
+        planoCoordenado(gc);
+        /*int i;
         Color color = new Color(50, 26, 94);
         if (vectorPuntos != null) {
             for(i = 0; i < vectorPuntos.size() - 1; i++) {
@@ -33,7 +56,7 @@ public class Dibujo extends Canvas {
             gc.drawLine(vectorPuntos.get(i).getX(), vectorPuntos.get(i).getY(), 
                             vectorPuntos.get(0).getX(), vectorPuntos.get(0).getY());
                             
-        }
+        } */
 
     }
     
