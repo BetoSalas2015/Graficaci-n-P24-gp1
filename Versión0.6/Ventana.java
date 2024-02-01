5bcb032a7f4aed1755bdef3496b1e52ce82b32ac
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2024-01-31 18:49:10 -0600

    Sesión 8 - 31/01/2024

--- Versión0.6/Ventana.java
+++ Versión0.6/Ventana.java
@@ -1,7 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
-import java.io.*;
+import java.util.Vector;
 
 public class Ventana extends Frame {
 
@@ -9,7 +8,7 @@ public class Ventana extends Frame {
     private Panel panelDatos, panelPunto1;
     private Label lblPunto1;
     private TextField txtPunto1x, txtPunto1y;
-    private Button btnGraficar, btnAgregar, btnCargar;
+    private Button btnGraficar, btnAgregar;
     private Vector<Point> vectorPuntos;
 
     public Ventana() {
@@ -29,32 +28,29 @@ public class Ventana extends Frame {
         txtPunto1x = new TextField("0");
         txtPunto1y = new TextField("0");
 
-        //btnAgregar = new Button("Agregar");
-        btnCargar = new Button("Cargar Puntos");
+        btnAgregar = new Button("Agregar");
         btnGraficar = new Button("Graficar");
 
-        panelPunto1.add(txtPunto1x); 
+        panelPunto1.add(txtPunto1x);
         panelPunto1.add(txtPunto1y);
 
 
         panelDatos.add(lblPunto1);
         panelDatos.add(panelPunto1);
-        //panelDatos.add(btnAgregar);
+        panelDatos.add(btnAgregar);
 
         dibujo = new Dibujo();
 
         add(dibujo, "Center"); // agraga el objeto dibujo a la ventana
-        //add(panelDatos, "North");
-        //add(btnGraficar, "South"); 
-        add(btnCargar, "South");
+        add(panelDatos, "North");
+        add(btnGraficar, "South"); 
         setSize(400, 400);
         setVisible(true);
         setResizable(true);
 
         addWindowListener(new CloseWin());
         btnGraficar.addActionListener( new BotónGraficar() );
-        //btnAgregar.addActionListener(new BotónAgregar() );
-        btnCargar.addActionListener(new BotónCargar());
+        btnAgregar.addActionListener(new BotónAgregar() );
     }
 
     private class CloseWin extends WindowAdapter {
@@ -80,31 +76,6 @@ public class Ventana extends Frame {
         public void actionPerformed(ActionEvent e) {
             vectorPuntos.add( new Point(Integer.parseInt( txtPunto1x.getText()), 
                                         Integer.parseInt(txtPunto1y.getText())));
-        }
-    }
-
-    private class BotónCargar implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String c, cad1, cad2; 
-            StringTokenizer token;
-            vectorPuntos = new Vector<Point>();
-            try {
-                BufferedReader entrada = new BufferedReader( new FileReader("coordenadas.txt"));
-                while (( c = entrada.readLine() ) != null) {
-                    token = new StringTokenizer(c, ",");
-                    cad1 = token.nextToken();
-                    cad2 = token.nextToken();
-
-                    vectorPuntos.add( new Point( Integer.parseInt(cad1), Integer.parseInt(cad2) ));
-                }
-                entrada.close();
-                dibujo.asignaPuntos(vectorPuntos);
-                dibujo.repaint();
-            } catch (IOException ex) {
-                System.out.println("El archivo no se pudi cargar. ");
-            }
-            
         }
     }
 
